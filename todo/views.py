d99097db9cff4420fa6a5c921d6fb797a390d25a
Kay <info@beckmann-md.de>
2023-11-20 19:57:22 +0100

    add first API-Route

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows todos to be viewed or edited.
+  """
+  queryset = Todo.objects.all().order_by('id')
+  serializer_class = TodoSerializer
+  permission_classes = [] #permissions.IsAuthenticated]
