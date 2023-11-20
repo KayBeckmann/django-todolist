91e9fc4acf1633639d63ff75b0e6503e03679158
Kay <info@beckmann-md.de>
2023-11-20 18:48:35 +0100

    todo model

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import datetime
 
 # Create your models here.
-class Todo(models.Models):
+class Todo(models.Model):
   title = models.CharField(max_length=30)
   description = models.CharField(max_length=30)
   created_at = models.DateField(default=datetime.date.today)
