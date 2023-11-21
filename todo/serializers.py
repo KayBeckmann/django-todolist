f1257de317f1ece570d2bd426ef6c7810184128f
Kay <info@beckmann-md.de>
2023-11-21 19:19:34 +0100

    edit User serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,7 +5,7 @@ from .models import Todo
 class UserSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = User
-    fields = ['id']
+    fields = ['id', 'username']
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
   user = UserSerializer()
