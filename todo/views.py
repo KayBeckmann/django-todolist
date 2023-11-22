73eab7a0232738f1cfd2176baa9190b9163ec5ca
Kay <info@beckmann-md.de>
2023-11-22 20:49:03 +0100

    add creat todo

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
+from django.core import serializers
+from rest_framework import viewsets, permissions
 from .models import Todo
 from .serializers import TodoSerializer
+from django.http import HttpResponse
 
 class TodoViewSet(viewsets.ModelViewSet):
   """
@@ -11,3 +12,8 @@ class TodoViewSet(viewsets.ModelViewSet):
   queryset = Todo.objects.all().order_by('id')
   serializer_class = TodoSerializer
   permission_classes = [] #permissions.IsAuthenticated]
+  
+  def create(self, request):
+    todo = Todo.objects.create(title=request.POST.get('title'), description = request.POST.get('description'), user = request.user)
+    serialized_obj = serializers.serialize('json', [todo, ])
+    return HttpResponse(serialized_obj, content_type='application/json')
